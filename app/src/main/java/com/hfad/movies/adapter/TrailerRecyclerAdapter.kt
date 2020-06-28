226e490c20fd44e386c65edb79227af4c9b16eec
AlenNka333 <elena.walst@bk.ru>
2020-06-28 19:57:17 +0300

    Add TrailerRecyclerAdapter

---
+++ app/src/main/java/com/hfad/movies/adapter/TrailerRecyclerAdapter.kt
@@ -0,0 +1,26 @@
+package com.hfad.movies.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TrailerRecyclerAdapter(): RecyclerView.Adapter<TrailerRecyclerAdapter.TrailerHolder>() {
+
+
+
+    inner class TrailerHolder(v: View): RecyclerView.ViewHolder(v) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrailerHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TrailerHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
